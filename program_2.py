a4cc900ecf1141627298d94d7170b7be29a353f7
Teresa336 <tpfischer@students.unwsp.edu>
2024-09-05 09:46:42 -0500

    Add files via upload

--- program_2.py
+++ program_2.py
@@ -1,6 +1,9 @@
-def proverbs_22_29():
-    # Write your Proverbs 22:29 code below
-    print('...')
-    
+#Programmer: Teresa Fischer
+#Date: September 5, 2024
+#Title: Program A: Modified Hello Part II
 
-proverbs_22_29()
+#Display Text
+print("\"Do you see a man skillful in his work?")
+print("He will stand before kings;")
+print("he will not stand before obscure men.\"")
+print((' '*4), '(Proverbs 22:29)')
